871180a725546b9e72767499d549e0a27dece479
Sudipta <sudiptabala32@gmail.com>
2020-11-07 07:37:33 +0530

    [Sudipta] Add : Adding hotel to hotel reservation system

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -6,7 +6,7 @@ namespace HotelReservationSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hotel Reservation Program.");
+            Console.WriteLine("Welcome to the Hotel Reservation System.");
         }
     }
 }
